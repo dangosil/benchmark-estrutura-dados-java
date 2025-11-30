de7de4534317bedd87ec5644c8e16563cb9a675b
Daniel <dangosil@yahoo.com.br>
2025-11-30 18:17:56 -0300

    feat: implementa gerador de dados para os testes
    add método para gerar vetores ordenados, inversamente ordenados e aleatórios

--- util/GeradorDados.java
+++ util/GeradorDados.java
@@ -1,5 +1,35 @@
 package util;
 
+import java.util.Random;
+
 public class GeradorDados {
-    
+
+    // vetor ordenado
+    public static int[] gerarOrdenado(int tamanho) {
+        int[] vetor = new int[tamanho];
+        for(int i = 0 ; i< tamanho; i++) {
+            vetor[i] = i;
+        }
+        return vetor;
+    }
+
+    // vetor inversamente ordenado
+    public static int[] gerarInversamenteOrdenado(int tamanho) {
+        int[] vetor = new int[tamanho];
+        for(int i = 0; i < tamanho; i++) {
+            vetor[i] = tamanho - i;
+        }
+        return vetor;
+    }
+
+    // vetor aleatório
+    public static int[] gerarAleatorio(int tamanho) {
+        int[] vetor = new int[tamanho];
+        Random random = new Random();
+
+        for(int i = 0; i < tamanho; i++) {
+            vetor[i] = random.nextInt(tamanho * 2);
+        }
+        return vetor;
+    }
 }
