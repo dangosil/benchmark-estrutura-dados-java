bc5eac6e4a518c08707c87a043ff3d2d9e7d0c39
Daniel <dangosil@yahoo.com.br>
2025-12-05 02:52:21 -0300

    feat: implementa QuickSort e add resultados finais no Benchmark

--- Benchmark.java
+++ Benchmark.java
@@ -1,16 +1,17 @@
 import java.util.Random;
-import java.util.Arrays;
-
 import estruturas.Vetor;
 import estruturas.ArvoreBinaria;
 import estruturas.ArvoreAVL;
 import util.GeradorDados;
+import ordenacao.QuickSort;
 import ordenacao.SelectionSort;
 import util.TempoExecucao;
+
 public class Benchmark {
 
     private static final int[] TAMANHOS = { 100, 1000, 10000 };
     private static final int REPETICOES = 5;
+
     public static void main(String[] args) {
         System.out.println("=== BENCHMARK: ESTRUTURAS DE DADOS ===");
 
@@ -27,7 +28,8 @@ public class Benchmark {
 
     private static void executarBateriaInsercao() {
         System.out.println(">>> 1. TEMPO DE INSERCAO (Medio de 5 execucoes) <<<");
-        System.out.printf("%-8s | %-10s | %-12s | %-12s | %-12s\n", "Qtd", "Cenario", "Vetor (ns)", "ABB (ns)", "AVL (ns)");
+        System.out.printf("%-8s | %-10s | %-12s | %-12s | %-12s\n", "Qtd", "Cenario", "Vetor (ns)", "ABB (ns)",
+                "AVL (ns)");
         System.out.println("--------------------------------------------------------------------------");
 
         String[] cenarios = { "Aleatorio", "Ordenado", "Inverso" };
@@ -43,57 +45,92 @@ public class Benchmark {
                     // Teste Vetor
                     Vetor vetor = new Vetor();
                     timer.iniciar();
-                    for (int v : dados) vetor.adicionar(v);
+                    for (int v : dados)
+                        vetor.adicionar(v);
                     somaVetor += timer.parar();
 
                     // Teste ABB
                     ArvoreBinaria abb = new ArvoreBinaria();
                     timer.iniciar();
-                    for (int v : dados) abb.inserir(v);
+                    for (int v : dados)
+                        abb.inserir(v);
                     somaABB += timer.parar();
 
                     // Teste AVL
                     ArvoreAVL avl = new ArvoreAVL();
                     timer.iniciar();
-                    for (int v : dados) avl.inserir(v);
+                    for (int v : dados)
+                        avl.inserir(v);
                     somaAVL += timer.parar();
                 }
-                
-                System.out.printf("%-8d | %-10s | %-12d | %-12d | %-12d\n", 
-                    tamanho, cenario, (somaVetor/REPETICOES), (somaABB/REPETICOES), (somaAVL/REPETICOES));
+
+                System.out.printf("%-8d | %-10s | %-12d | %-12d | %-12d\n",
+                        tamanho, cenario, (somaVetor / REPETICOES), (somaABB / REPETICOES), (somaAVL / REPETICOES));
             }
             System.out.println("--------------------------------------------------------------------------");
         }
     }
 
     private static void executarBateriaOrdenacao() {
-        System.out.println(">>> 2. TESTES DE ORDENACAO (Selection Sort) <<<");
-        System.out.printf("%-10s | %-15s | %-15s\n", "Tamanho", "Cenario", "Tempo Medio");
-        System.out.println("---------------------------------------------------------");
+        System.out.println(">>> 3. TESTES DE ORDENACAO (Selection vs QuickSort) <<<");
+        System.out.printf("%-10s | %-12s | %-15s | %-15s\n", "Tamanho", "Cenario", "Selection(ns)", "QuickSort(ns)");
+        System.out.println("----------------------------------------------------------------------");
 
-        String[] cenarios = {"Aleatorio", "Ordenado", "Inverso"};
+        String[] cenarios = { "Aleatorio", "Ordenado", "Inverso" };
+        TempoExecucao timer = new TempoExecucao();
 
         for (int tamanho : TAMANHOS) {
             for (String cenario : cenarios) {
-                long media = medirOrdenacao(tamanho, cenario);
-                System.out.printf("%-10d | %-15s | %-15d ns\n", tamanho, cenario, media);
+                long somaSelection = 0;
+                long somaQuick = 0;
+
+                for (int i = 0; i < REPETICOES; i++) {
+                    int[] dadosBase = obterDadosPorCenario(tamanho, cenario);
+
+                    // Teste Selection Sort
+                    Vetor v1 = new Vetor();
+                    for (int v : dadosBase)
+                        v1.adicionar(v);
+
+                    SelectionSort selSort = new SelectionSort();
+                    timer.iniciar();
+                    selSort.ordenar(v1);
+                    somaSelection += timer.parar();
+
+                    // Teste QuickSort
+                    Vetor v2 = new Vetor();
+                    for (int v : dadosBase)
+                        v2.adicionar(v);
+
+                    QuickSort qSort = new QuickSort();
+                    timer.iniciar();
+                    try {
+                        qSort.ordenar(v2);
+                    } catch (StackOverflowError e) {
+                        System.out.println("[Estouro de Pilha]");
+                    }
+                    somaQuick += timer.parar();
+                }
+
+                System.out.printf("%-10d | %-12s | %-15d | %-15d\n",
+                        tamanho, cenario, (somaSelection / REPETICOES), (somaQuick / REPETICOES));
             }
-            System.out.println("---------------------------------------------------------");
+            System.out.println("----------------------------------------------------------------------");
         }
     }
 
     private static void executarBateriaBusca() {
         System.out.println(">>> 3. TESTES DE BUSCA (Vetor vs ABB vs AVL) <<<");
-        System.out.printf("%-8s | %-12s | %-10s | %-10s | %-10s | %-10s\n", 
-            "Qtd", "Alvo", "Vet(Sequencial)", "Vet(Binaria)", "ABB", "AVL");
-        System.out.println("--------------------------------------------------------------------------------");
+        System.out.printf("%-8s | %-12s | %-16s | %-12s | %-10s | %-10s\n",
+                "Qtd", "Alvo", "Vet(Sequencial)", "Vet(Binaria)", "ABB", "AVL");
+        System.out.println("---------------------------------------------------------------------------------------");
 
         Random random = new Random();
         TempoExecucao timer = new TempoExecucao();
 
         for (int tamanho : TAMANHOS) {
             int[] dados = GeradorDados.gerarOrdenado(tamanho);
-            
+
             Vetor vetor = new Vetor();
             ArvoreBinaria abb = new ArvoreBinaria();
             ArvoreAVL avl = new ArvoreAVL();
@@ -104,15 +141,21 @@ public class Benchmark {
                 avl.inserir(v);
             }
 
-            int indexRand = random.nextInt(tamanho);
-            int[] alvos = { 
-                dados[0],
-                dados[tamanho - 1],
-                dados[indexRand],
-                -1337
+            int idxRand1 = random.nextInt(tamanho);
+            int idxRand2 = random.nextInt(tamanho);
+            int idxRand3 = random.nextInt(tamanho);
+
+            int[] alvos = {
+                    dados[0],
+                    dados[tamanho / 2],
+                    dados[tamanho - 1],
+                    dados[idxRand1],
+                    dados[idxRand2],
+                    dados[idxRand3],
+                    -1337
             };
 
-            String[] nomes = { "Primeiro", "Ultimo", "Aleatorio", "Inexistente" };
+            String[] nomes = { "Primeiro", "Meio", "Ultimo", "Aleat. 1", "Aleat. 2", "Aleat. 3", "Inexistente" };
 
             for (int i = 0; i < alvos.length; i++) {
                 int alvo = alvos[i];
@@ -120,48 +163,43 @@ public class Benchmark {
 
                 for (int r = 0; r < REPETICOES; r++) {
                     // Vetor Sequencial
-                    timer.iniciar(); vetor.buscaSequencial(alvo); tSeq += timer.parar();
-                    
+                    timer.iniciar();
+                    vetor.buscaSequencial(alvo);
+                    tSeq += timer.parar();
+
                     // Vetor Binária
-                    timer.iniciar(); vetor.pesquisaBinaria(alvo); tBin += timer.parar();
+                    timer.iniciar();
+                    vetor.pesquisaBinaria(alvo);
+                    tBin += timer.parar();
 
                     // Arvore Binária
-                    timer.iniciar(); abb.buscar(alvo); tABB += timer.parar();
+                    timer.iniciar();
+                    abb.buscar(alvo);
+                    tABB += timer.parar();
 
                     // Arvore AVL
-                    timer.iniciar(); avl.buscar(alvo); tAVL += timer.parar();
+                    timer.iniciar();
+                    avl.buscar(alvo);
+                    tAVL += timer.parar();
                 }
 
-                System.out.printf("%-8d | %-12s | %-10d | %-10d | %-10d | %-10d\n",
-                    tamanho, nomes[i], (tSeq/REPETICOES), (tBin/REPETICOES), (tABB/REPETICOES), (tAVL/REPETICOES));
+                System.out.printf("%-8d | %-12s | %-16d | %-12d | %-10d | %-10d\n",
+                        tamanho, nomes[i], (tSeq / REPETICOES), (tBin / REPETICOES), (tABB / REPETICOES),
+                        (tAVL / REPETICOES));
             }
-            System.out.println("--------------------------------------------------------------------------------");
+            System.out
+                    .println("---------------------------------------------------------------------------------------");
         }
-    }
-
-    private static long medirOrdenacao(int tamanho, String cenario) {
-        long soma = 0;
-        TempoExecucao timer = new TempoExecucao();
-
-        for (int i = 0; i < REPETICOES; i++) {
-            Vetor vetor = new Vetor();
-            int[] dados = obterDadosPorCenario(tamanho, cenario);
-            for (int v : dados) vetor.adicionar(v);
-            
-            SelectionSort sort = new SelectionSort();
-
-            timer.iniciar();
-            sort.ordenar(vetor);
-            soma += timer.parar();
-        }
-        return soma / REPETICOES;
     }
 
     private static int[] obterDadosPorCenario(int tamanho, String cenario) {
         switch (cenario) {
-            case "Ordenado": return GeradorDados.gerarOrdenado(tamanho);
-            case "Inverso": return GeradorDados.gerarInversamenteOrdenado(tamanho);
-            default: return GeradorDados.gerarAleatorio(tamanho);
+            case "Ordenado":
+                return GeradorDados.gerarOrdenado(tamanho);
+            case "Inverso":
+                return GeradorDados.gerarInversamenteOrdenado(tamanho);
+            default:
+                return GeradorDados.gerarAleatorio(tamanho);
         }
     }
 }
--- ordenacao/QuickSort.java
+++ ordenacao/QuickSort.java
@@ -1,5 +1,42 @@
 package ordenacao;
 
+import estruturas.Vetor;
+
 public class QuickSort {
-    
+    public void ordenar(Vetor vetor) {
+        int n = vetor.getTamanho();
+        
+        quickSort(vetor, 0, n - 1);
+    }
+
+    private void quickSort(Vetor vetor, int inicio, int fim) {
+        if (inicio < fim) {
+            int indicePivo = particionar(vetor, inicio, fim);
+
+            quickSort(vetor, inicio, indicePivo - 1);
+            quickSort(vetor, indicePivo + 1, fim);
+        }
+    }
+
+    private int particionar(Vetor vetor, int inicio, int fim) {
+        int pivo = vetor.pegarNoIndice(fim);
+        
+        int i = (inicio - 1);
+
+        for (int j = inicio; j < fim; j++) {
+            if (vetor.pegarNoIndice(j) <= pivo) {
+                i++;
+                trocar(vetor, i, j);
+            }
+        }
+        trocar(vetor, i + 1, fim);
+        return i + 1;
+    }
+
+    private void trocar(Vetor vetor, int i, int j) {
+        int temp = vetor.pegarNoIndice(i);
+        
+        vetor.colocarNoIndice(i, vetor.pegarNoIndice(j));
+        vetor.colocarNoIndice(j, temp);
+    }
 }
